8feddfd379905fd50f5ea65ced57078dae18ca97
Atul Bari <atul.bari@novanet.net>
2021-11-15 20:20:39 +0530

    env support

--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module testmongo
 
 go 1.14
 
-require go.mongodb.org/mongo-driver v1.5.3
+require (
+	github.com/joho/godotenv v1.3.0
+	go.mongodb.org/mongo-driver v1.5.3
+)
--- main.go
+++ main.go
@@ -2,24 +2,30 @@ package main
 
 import (
 	"context"
-	"fmt"
+	"log"
+	"os"
 	"time"
 
+	"github.com/joho/godotenv"
 	"go.mongodb.org/mongo-driver/bson"
 )
 
 var database *MongoConn
 
 func main() {
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
 	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
 	defer cancel()
-	database = NewMongoConnection("", ctx)
+	database = NewMongoConnection(os.Getenv("MONGO_URI"), ctx)
 	defer database.client.Disconnect(ctx)
 
 	dbs, _ := database.client.ListDatabaseNames(ctx, bson.M{})
-	fmt.Println(dbs)
+	log.Println(dbs)
 
 	// fmt.Println(database.InsertMillion())
 	data, _ := database.Find("Jon")
-	fmt.Println(data)
+	log.Println(data)
 }
